7b56aec2c8513185453019fcc58c16acbba3af7f
kali <kali@MacBook-Air-kali.local>
2024-09-02 16:22:18 +0800

    fix

--- index.b9f60.js
+++ index.b9f60.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.3f5e2.js"], function (_export, _context) {
   "use strict";
 
   var Application, application;
